79ad18ae83029f3e57308b21f54d95f8c4ac3a90
lucas.castro <lucas.castro@cxcontact.com.br>
2026-03-01 21:03:15 -0300

    feat: implementando consumo do banco, no caso uma lista em memoria.

---
+++ src/SubscripManager.infra/Repositories/SignatureRepository.cs
@@ -0,0 +1,79 @@
+﻿using SubscripManager.domain.Entities;
+using SubscripManager.domain.Entities.Enum;
+using SubscripManager.domain.Interfaces;
+
+namespace SubscripManager.infra.Repositories
+{
+    public class SignatureRepository : ISignatureRepository
+    {
+        private readonly List<Signature> _signatures = new();
+        public Signature Create(Signature signature)
+        {
+            try
+            {
+                if(!_signatures.Any(x=>x.Id == signature.Id))
+                    _signatures.Add(signature);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Falha ao criar usuário.", ex);
+            }
+
+            return signature;
+        }
+
+        public Signature GetById(Guid id)
+        {
+            try
+            {
+                return _signatures.FirstOrDefault(x=>x.Id == id);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public List<Signature> GetByUserId(Guid userId)
+        {
+            try
+            {
+                return _signatures.Where(x => x.UserId == userId).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public List<Signature> GetByUserIdAndByStatus(Guid userId, Status status)
+        {
+            try
+            {
+                return _signatures.Where(x => x.UserId == userId && x.Status == status).ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public Signature Update(Guid id, Signature signatures)
+        {
+            try
+            {
+                var signatureItem = _signatures.FirstOrDefault(x => x.Id == id);
+                if (signatureItem == null) return null;
+
+                var idx = _signatures.IndexOf(signatureItem);
+                _signatures[idx] = signatures;
+
+                return _signatures[idx];
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ src/SubscripManager.infra/Repositories/UserRepository.cs
@@ -0,0 +1,62 @@
+﻿using SubscripManager.domain.Entities;
+using SubscripManager.domain.Interfaces;
+
+namespace SubscripManager.infra.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly List<User> _user = new();
+        public User Create(User user)
+        {
+            try
+            {
+                if (!_user.Any(x => x.Id == user.Id))
+                    _user.Add(user);
+            }
+            catch { throw; }
+
+            return user;
+        }
+
+        public User GetUserById(Guid id)
+        {
+            try
+            {
+                return _user.FirstOrDefault(x => x.Id == id);
+            }
+            catch { 
+                throw; 
+            }
+        }
+
+        public List<User> GetUsers()
+        {
+            try
+            {
+                return _user;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public User Update(Guid id, User user)
+        {
+            try
+            {
+                var userItem = _user.FirstOrDefault(x => x.Id == id);
+                if (userItem == null) return null;
+
+                var idx = _user.IndexOf(userItem);
+                _user[idx] = user;
+
+                return _user[idx];
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
